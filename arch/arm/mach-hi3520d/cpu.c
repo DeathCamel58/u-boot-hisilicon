0fcb94e49d131bc21fc3249bfeed333aff655269
Dylan Corrales <deathcamel58@gmail.com>
2023-10-20 12:27:57 -0400

    Add `UCLASS_SOC` driver
    This driver can determine which SoC model and revision number U-Boot is currently running on.

--- arch/arm/mach-hi3520d/cpu.c
+++ arch/arm/mach-hi3520d/cpu.c
@@ -1,9 +1,36 @@
 #include <common.h>
+#include <soc.h>
 #include <asm/io.h>
+#include <asm/arch/platform.h>
+#include <dm.h>
 
-void sdelay(unsigned long loops)
+#if CONFIG_DISPLAY_CPUINFO
+int print_cpuinfo(void)
 {
-    __asm__ volatile ("1:\n"
-                      "subs %0, %1, #1\n"
-                      "bne 1b" : "=r" (loops) : "0" (loops));
+    struct udevice *soc;
+    char name[64];
+    int ret;
+
+    printf("SoC:   ");
+
+    ret = soc_get(&soc);
+    if (ret) {
+        printf("UNKNOWN: Device Tree Error: %d\n", ret);
+        return 0;
+    }
+
+    ret = soc_get_family(soc, name, 64);
+    if (!ret) {
+        printf("%s ", name);
+    }
+
+    ret = soc_get_revision(soc, name, 64);
+    if (!ret) {
+        printf("%s ", name);
+    }
+
+    printf("\n");
+
+    return 0;
 }
+#endif
